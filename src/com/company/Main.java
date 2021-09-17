b9c7b4481207ca546f04d7dccb876d14f4f29498
BuildTools <unconfigured@null.spigotmc.org>
2021-09-17 02:24:07 -0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,75 @@
+package com.company;
+
+import java.util.Locale;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+	    while(true){
+            int total = 0;
+
+            Scanner input = new Scanner(System.in);
+            System.out.println("How many dice would you like to roll (In the XdY format please, there is no Input protection)?");
+            System.out.print("Your Input: ");
+            String rawInput = input.next();
+
+            //accounting for the first change of index
+            int index = -1;
+
+            for(int i =0; i < rawInput.length(); i++){
+                index = index +1;
+                char dFinder = rawInput.charAt(i);
+                if(dFinder == 'd'){
+                    //stop the loop
+                    break;
+                }
+            }
+
+            String rawInput1 = rawInput.substring(0,index);
+            String rawInput2 = rawInput.substring(index+1);
+
+            int diceNum = 0;
+            int diceSides = 0;
+
+            diceNum = Integer.valueOf(rawInput1);
+            diceSides = Integer.valueOf(rawInput2);
+
+            int[] dice = new int[diceNum];
+
+            for(int i = 0; i < diceNum;i++){
+                Random rand = new Random();
+                dice[i] = rand.nextInt(diceSides) + 1;
+                System.out.println("You Rolled a "+dice[i]+"!");
+                total += dice[i];
+            }
+            System.out.println("Your Total is: "+total);
+
+            System.out.println("\nWould you like to go again? (y/n)?");
+            System.out.print("Your Input: ");
+            String continueLoop = input.next();
+            continueLoop.toLowerCase(Locale.ROOT);
+
+            while(true) {
+                if (continueLoop.equalsIgnoreCase("y")) {
+                    break;
+                } else if (continueLoop.equalsIgnoreCase("n")) {
+                    break;
+                } else {
+                  System.out.println("That was not a valid output, Please try Again");
+                  System.out.print("Your Input: ");
+                  continueLoop = input.next();
+                }
+            }
+            //Break out of big while loop
+            if (continueLoop.equalsIgnoreCase("n")) {
+                break;
+            }
+            
+            System.out.println(" ");
+        }
+
+    }
+}
